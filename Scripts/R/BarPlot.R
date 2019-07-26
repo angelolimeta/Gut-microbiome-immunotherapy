24d1edcbbb8bd7956b42d7470ff6d94d6f6268bd
Angelo Limeta <angelol@student.chalmers.se>
2019-07-26 13:43:44 +0200

    Revert "chore: transfer download scripts to new directory"
    This reverts commit b4275bc6e116863139bc320e6b3b2b5a3383f79f.

--- Scripts/R/BarPlot.R
+++
@@ -1,8 +0,0 @@
-# ==== IMPORT DATA ====
-library("phyloseq")
-# Import Phyloseq object for all studies
-physeq = readRDS(file = "/Users/angelol/Documents/PhD/Gut-microbiome-immunotherapy/data/Abundance_tables/physeq.rds")
-
-physeq  = transform_sample_counts(physeq, function(x) x / sum(x) )
-physeq = filter_taxa(physeq, function(x) mean(x) > 1e-5, TRUE)
-plot_bar(physeq,x="Response", fill = "Phylum")
