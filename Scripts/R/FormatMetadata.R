bdb55bd231ee5d367186ca3fad5a0979de05908d
Angelo Limeta <angelol@student.chalmers.se>
2019-08-15 17:10:55 +0200

    Feat: Add and process metadata for Routy et al data

--- Scripts/R/FormatMetadata.R
+++ Scripts/R/FormatMetadata.R
@@ -6,30 +6,39 @@
 ##################################################
 
 # == LOAD DATA ==
+library(readr)
 
 # Study metadata
-library(readr)
 Gop_meta_full <- data.frame(read_delim("/Users/angelol/Documents/PhD/Gut-microbiome-immunotherapy/Metadata/Gop_meta.csv", 
                        ";", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE),
-                       stringsAsFactors = FALSE)
+                        stringsAsFactors = FALSE)
 
 Mat_meta_full <- data.frame(read_csv("/Users/angelol/Documents/PhD/Gut-microbiome-immunotherapy/Metadata/Mat_meta.csv"),
-                       stringsAsFactors = FALSE)
+                        stringsAsFactors = FALSE)
 
 Fra_meta_full <- data.frame(read_delim("/Users/angelol/Documents/PhD/Gut-microbiome-immunotherapy/Metadata/Fra_meta.csv", 
                         "\t", escape_double = FALSE, trim_ws = TRUE),
-                       stringsAsFactors = FALSE)
+                        stringsAsFactors = FALSE)
+
+Rou_meta_full <- data.frame(read_delim("~/Documents/PhD/Gut-microbiome-immunotherapy/Metadata/Rou_meta.tsv", 
+                        "\t", escape_double = FALSE, trim_ws = TRUE),
+                        stringsAsFactors = FALSE)
+
 # Sample data
 Gop_sampInfo = data.frame(read_delim("~/Documents/PhD/Gut-microbiome-immunotherapy/Metadata/PRJEB22893.txt", 
                         "\t", escape_double = FALSE, trim_ws = TRUE),
                         stringsAsFactors = FALSE)
 
 Mat_sampInfo = data.frame(read_delim("~/Documents/PhD/Gut-microbiome-immunotherapy/Metadata/PRJNA399742.txt", 
-                                     "\t", escape_double = FALSE, trim_ws = TRUE),
+                        "\t", escape_double = FALSE, trim_ws = TRUE),
                         stringsAsFactors = FALSE)
 
 Fra_sampInfo = data.frame(read_delim("~/Documents/PhD/Gut-microbiome-immunotherapy/Metadata/PRJNA397906.txt", 
-                                     "\t", escape_double = FALSE, trim_ws = TRUE),
+                        "\t", escape_double = FALSE, trim_ws = TRUE),
+                        stringsAsFactors = FALSE)
+
+Rou_sampInfo = data.frame(read_delim("~/Documents/PhD/Gut-microbiome-immunotherapy/Metadata/PRJEB22863.txt", 
+                        "\t", escape_double = FALSE, trim_ws = TRUE),
                         stringsAsFactors = FALSE)
 
 # == TIDY DATA ==
@@ -44,14 +53,16 @@ for (i in 2:ncol(Gop_meta_full)) {
 # Extract common data
 Gop_meta = Gop_meta_full[,c("subject_id","phenotype","treatment")]
 Gop_meta = cbind(Gop_meta,rep("Gopalakrishnan_et_al",nrow(Gop_meta)))
-colnames(Gop_meta) = c("Study_patient_id","Response","Treatment","Study")
+Gop_meta = cbind(Gop_meta,rep("MM",nrow(Gop_meta)))
+colnames(Gop_meta) = c("Study_patient_id","Response","Treatment","Study","Cancer_type")
 
 # Mat_meta
 Mat_meta = Mat_meta_full[,c("Sample","Response")]
 Mat_meta = cbind(Mat_meta,rep("Anti-PD1",nrow(Mat_meta)),rep("Matson_et_al",nrow(Mat_meta)))
+Mat_meta = cbind(Mat_meta,rep("MM",nrow(Mat_meta)))
 Mat_meta$Response = gsub("NonResponder","NR",Mat_meta$Response)
 Mat_meta$Response = gsub("Responder","R",Mat_meta$Response)
-colnames(Mat_meta) = c("Study_patient_id","Response","Treatment","Study")
+colnames(Mat_meta) = c("Study_patient_id","Response","Treatment","Study","Cancer_type")
 # Patient 09, 41 and 42 seems to only have Amplicon data. Exclude them from the dataset
 Mat_meta = Mat_meta[Mat_meta$Study_patient_id != "P09",]
 Mat_meta = Mat_meta[Mat_meta$Study_patient_id != "P41",]
@@ -60,7 +71,8 @@ Mat_meta = Mat_meta[Mat_meta$Study_patient_id != "P42",]
 # Fra_meta
 Fra_meta = Fra_meta_full[,c("Patient.Identifier","RECIST.Category","ICT.Therapy")]
 Fra_meta = cbind(Fra_meta,rep("Frankel_et_al",nrow(Fra_meta)))
-colnames(Fra_meta) = c("Study_patient_id","Response","Treatment","Study")
+Fra_meta = cbind(Fra_meta,rep("MM",nrow(Fra_meta)))
+colnames(Fra_meta) = c("Study_patient_id","Response","Treatment","Study","Cancer_type")
 Fra_meta$Response = gsub("Stable","NR",Fra_meta$Response)
 Fra_meta$Response = gsub("Progression","NR",Fra_meta$Response)
 Fra_meta$Response = gsub("Response","R",Fra_meta$Response)
@@ -69,8 +81,20 @@ Fra_meta$Treatment[which(Fra_meta$Treatment == "I")] = "Anti-CTLA4"
 Fra_meta$Treatment[which(Fra_meta$Treatment == "P")] = "Anti-PD1"
 Fra_meta$Treatment[which(Fra_meta$Treatment == "N")] = "Anti-PD1"
 
+# Rou_meta
+Rou_meta = Rou_meta_full[,c("Title","Response")]
+Rou_meta = cbind(Rou_meta,rep("Anti-PD1",nrow(Rou_meta)), rep("Routy_et_al", nrow(Rou_meta)))
+Rou_meta = cbind(Rou_meta,Rou_meta_full$host.disease.status)
+# Remove T1 samples
+Rou_meta = Rou_meta[grepl("T0",Rou_meta[,1]),] 
+colnames(Rou_meta) = c("Study_patient_id","Response","Treatment","Study","Cancer_type")
+Rou_meta$Response = gsub("Partial response","R",Rou_meta$Response)
+Rou_meta$Response = gsub("Stable","NR",Rou_meta$Response)
+Rou_meta$Response = gsub("Progression","NR",Rou_meta$Response)
+Rou_meta$Response = gsub("Dead","NR",Rou_meta$Response)
+
 # Combine all metadata
-clin = rbind(Gop_meta,Mat_meta,Fra_meta)
+clin = rbind(Gop_meta,Mat_meta,Fra_meta,Rou_meta)
 rownames(clin) = NULL
 
 # Create unique identifier for each patient, e.g p001_Gop_NR
@@ -86,7 +110,9 @@ for (i in 1:nrow(clin)) {
   if(clin$Study[i] == "Frankel_et_al"){
     Patient_id[i] = paste("p",sprintf("%03d",i),"_Fra_",clin$Response[i],sep = "")
   }
-  
+  if(clin$Study[i] == "Routy_et_al"){
+    Patient_id[i] = paste("p",sprintf("%03d",i),"_Rou_",clin$Response[i],sep = "")
+  }
 }
 
 clin = cbind(Patient_id,clin)
@@ -94,6 +120,9 @@ clin = cbind(Patient_id,clin)
 # Remove non-WGS samples from Matson_et_al data
 Mat_sampInfo = Mat_sampInfo[Mat_sampInfo$library_strategy == "WGS",]
 Mat_sampInfo = Mat_sampInfo[,-2]
+
+# Remove T1 samples from Routy et al data
+Rou_sampInfo = Rou_sampInfo[grepl("T0",Rou_sampInfo$sample_alias),]
 
 # Discard the 5 repeat samples within 1 month of starting ICT from the Frankel_et_al data.
 Fra_sampInfo$sample_alias = gsub("melanoma_","",Fra_sampInfo$sample_alias)
@@ -112,6 +141,9 @@ for (i in 1:nrow(clin)) {
   }
   if(clin$Study[i] == "Frankel_et_al"){
     Sample_id[i] = Fra_sampInfo$run_accession[which(clin$Study_patient_id[i] == Fra_sampInfo$sample_alias)]
+  }
+  if(clin$Study[i] == "Routy_et_al"){
+    Sample_id[i] = Rou_sampInfo$run_accession[which(clin$Study_patient_id[i] == Rou_sampInfo$sample_alias)]
   }
 }
 clin = cbind(Sample_id,clin)
